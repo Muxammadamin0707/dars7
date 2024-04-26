ed78b1f385283ec9a24d26051c1fb24d305bfd86
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-04-26 11:47:53 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,14 @@
+let muxammadamin = ["Muxammadamin", 12, true, "kak choh tepa"];
+
+console.log(muxammadamin);
+console.log(muxammadamin.length);
+
+let rost = confirm("Manzilling'iz to'g'rimi?");
+
+if (rost) {
+
+} else {
+    muxammadamin.pop();
+}
+
+console.log(muxammadamin);
